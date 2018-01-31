04e28f47ec77989bec152aa0b84cbb7288aa2f38
nooobguest <34414523+nooobguest@users.noreply.github.com>
2018-01-31 21:10:11 +0800

    Update example.py

--- example.py
+++ example.py
@@ -1,9 +1,8 @@
-import time
 from PIL import Image
-from slider import slider_cracker
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.action_chains import ActionChains
+
+from slider import slider_cracker
 
 
 
@@ -13,8 +12,6 @@ driver = webdriver.Chrome(executable_path='chromedriver.exe',
     chrome_options=chrome_options)'''#Chrome无头模式
 driver = webdriver.Chrome(executable_path='chromedriver.exe')
 driver.get('https://passport.bilibili.com/login')
-time.sleep(1)
-
 cracker = slider_cracker(driver)
 cracker.crack()
 
